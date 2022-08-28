6a9d9d7d0d00609b4bc0c18a60fb71fd97900af2
Lucas Menendez <epucas@gmail.com>
2022-08-28 21:58:03 +0200

    client documentation

--- pkg/sdk/client.go
+++ pkg/sdk/client.go
@@ -1,4 +1,6 @@
-// Package SDK
+// Package SDK allows to interact with paillier package easily, allowing to use
+// floating point and integer numbers with it, and supporting more operations
+// such as substraction and division over encrypted numbers.
 package sdk
 
 import (
@@ -8,12 +10,16 @@ import (
 	"github.com/lucasmenendez/gopaillier/pkg/paillier"
 )
 
-// Struct Client
+// Struct Client contains a Paillier key pair allowing to encrypt and decrypt
+// number.Number instances. Sharing Client.Key.PubKey (a paillier.PubKey
+// instance) with an external actor, it could compute operations over a
+// number.Number encrypted with the same paillier.PubKey.
 type Client struct {
 	Key *paillier.PrivateKey
 }
 
-// Function InitClient
+// Function InitClient returns a new client with a generated paillier.PrivKey
+// and paillier.PubKet pair with the size provided.
 func InitClient(keySize int) (*Client, error) {
 	var err error
 	var client = &Client{}
@@ -22,7 +28,9 @@ func InitClient(keySize int) (*Client, error) {
 	return client, err
 }
 
-// Function Encrypt
+// Function Encrypt returns the encrypted version of the provided number.Number.
+// It returns an error if the provided input is already encrypted or if some
+// error occurs during the input encryption process.
 func (client *Client) Encrypt(num *number.Number) (*number.Number, error) {
 	if num.IsEncrypted() {
 		return nil, errors.New("provided number is already encrypted")
@@ -34,7 +42,9 @@ func (client *Client) Encrypt(num *number.Number) (*number.Number, error) {
 	return result, err
 }
 
-// Function Decrypt
+// Function Decrypt returns the decrypted version of the provided number.Number.
+// It returns an error if the provided input is not encrypted or if some error
+// occurs during the input decryption process.
 func (client *Client) Decrypt(num *number.Number) (*number.Number, error) {
 	if !num.IsEncrypted() {
 		return nil, errors.New("provided number is not encrypted")
